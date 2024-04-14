1d6b164c4a227f029debb9e5fa2d6742480b147b
Swastik Vaish <swastiktba@gmail.com>
2024-04-14 21:52:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,10 +33,10 @@ st.markdown(
     )
 
 st.title('QUIC Probe')
-st.write('$$Network$$ $$Traffic$$ $$Classification$$ $$Tool$$')
+st.write('$$Network$$  $$Traffic$$  $$Classification$$  $$Tool$$')
 np.set_printoptions(suppress=True)
 # Load the model
-model = load_model("model.h5", compile=False)
+model = load_model("finalized_model_lgbm.sav", compile=False)
 
 # Load the labels
 class_names = open("labels.txt", "r").readlines()
@@ -56,16 +56,16 @@ if uploaded_file is not None:
     index = np.argmax(prediction)
     class_name = class_names[index]
     confidence_score = prediction[0][index]
-    foodname = class_name[2:].strip()
-    # Display the predicted Dish Name with Confidence Score
-    st.write(f'The Prediction is: $${foodname}$$')
+    trafficname = class_name[2:].strip()
+    # Display the predicted Traffic Name with Confidence Score
+    st.write(f'The Prediction is: $${trafficname}$$')
     st.write(f'Confidence %:', (confidence_score*100).round(3))
     # Useing Webscrap url to open the links
-    youtube_url = f"https://www.youtube.com/results?search_query=how+to+make+{foodname}"
-    blog_url = f"https://www.bbcgoodfood.com/search?q={foodname}"
+    youtube_url = f"https://www.youtube.com/watch?v=HnDsMehSSY4"
+    blog_url = f"https://www.chromium.org/quic/"
     
     
-    st.header(f'Checkout how to make {foodname} :')
+    st.header(f'What QUIC is :')
     col11, col12 = st.columns(2)
     with col11:
         st.write(f'Video')
@@ -76,67 +76,3 @@ if uploaded_file is not None:
         st.write(f'Blog')
         if st.button(f'Blog'):
             webbrowser.open(blog_url)
-        
-    st.header(f"$$Not$$ in mood to $$Cook$$,")
-    st.write(f"**Don't worry we got you Bro!**")
-    if st.button(f'Order $${foodname}$$ now!'):
-        # Launch the Chrome browser and navigate to Swiggy
-        driver = webdriver.Chrome(executable_path="chromedriver.exe")
-        driver.get("https://www.swiggy.com/")
-        # Wait for the page to load
-        time.sleep(5)
-        # Click on the "Locate Me" button
-        locate_button = driver.find_element(By.CLASS_NAME, "_1fiQt")
-        locate_button.click()
-        # Wait for the location to be detected
-        time.sleep(5)
-        # Get the current URL and append /search to it
-        current_url = driver.current_url
-        search_url = current_url + f"search?query={foodname}"
-        driver.get(search_url)
-        # Prompt the user to input ` to close the browser
-        keyboard.wait("`")
-        driver.quit()
-
-            
-
-    st.header(f'Ordered In $$Excess$$? Need to Throw it out!!!')
-    st.write(f'**$$Donate$$** Today and $$Save$$ $$Lives$$!')
-    st.write("Wastage for you is one time meal for someone else.")
-    if st.button(f'Donate Leftover Food!'):
-        # Create a new instance of the Chrome driver
-        driver = webdriver.Chrome(executable_path="chromedriver.exe")
-        # Navigate to Google
-        driver.get("https://www.google.com")
-
-        # Find the search box and enter a query
-        search_box = driver.find_element(By.NAME, 'q')
-        search_box.send_keys("ngo for food donation near me")
-        search_box.send_keys(Keys.RETURN)
-
-        # Wait for the results to load
-        time.sleep(10)
-
-        search_result = driver.find_element(By.CSS_SELECTOR, "a.yYlJEf.Q7PwXb.L48Cpd.brKmxb")
-
-        # Get the href attribute value of the link element
-        href = search_result.get_attribute("href")
-
-        # Open the href in a new tab
-        driver.execute_script("window.open('"+href+"', '_blank')")
-
-        # Prompt the user to input ` to close the browser
-        keyboard.wait("`")
-        driver.quit()
-    # Tells on different ways to save food
-    st.write("Here are some ways to **Save** **Food**:")
-
-    st.write("- Plan your meals ahead of time to avoid buying too much food")
-    st.write("- Store food properly to keep it fresh longer, such as using airtight containers or wrapping it tightly in plastic wrap")
-    st.write("- Freeze leftover food for future use")
-    st.write("- Use wilted vegetables or fruits in soups or smoothies")
-    st.write("- Make use of leftovers in new dishes, such as using leftover rice to make fried rice")
-    st.write("- Donate excess food to a food bank or shelter")
-    st.write("- Compost food scraps to reduce waste")
-    
-    st.write("By following these tips, we can help reduce food waste and make the most of the food we have.") 
